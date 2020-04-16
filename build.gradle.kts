88377db6ae330c69f0f769d7bf2d4c929e65a15a
VarnitS2 <varnits02@gmail.com>
2020-04-16 18:58:36 -0400

    Added dependencies, upgraded Gradle to 6.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "1.3.61"
+    application
+    id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
 }
 
 group = "org.example"
@@ -11,6 +13,10 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    implementation("com.github.doyaaaaaken:kotlin-csv-jvm:0.7.3")
+
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.0.2") // for kotest framework
+    testImplementation("io.kotest:kotest-assertions-core-jvm:4.0.2") // for kotest core jvm assertions
 }
 
 tasks {
@@ -20,4 +26,12 @@ tasks {
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
+}
+
+application {
+    mainClassName = "MainKt"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
