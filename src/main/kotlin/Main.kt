953e95374decf747309b58e1a8aed5702b068833
VarnitS2 <varnits02@gmail.com>
2020-04-16 22:35:43 -0400

    Created CSV-Worker for CSV functions.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,6 @@
-import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
-import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
-import java.io.File
-
-fun readFromCSV(fileName: String): List<Map<String, String>> {
-    return csvReader().readAllWithHeader(File(fileName))
-}
-
-fun writeToCSV(fileName: String, rows: List<List<String>>) {
-    csvWriter().open(fileName) { writeAll(rows) }
-}
-
-fun createNewCSV(rows: List<Map<String, String>>): List<List<String>> {
-    val newRows = mutableListOf<List<String>>()
-    newRows.add(listOf("name", "email", "netid"))
-
-    for (row in rows) {
-        if (!row["admin"]?.toBoolean()!!) {
-            val name: String = row["name"].toString()
-            val email: String = row["email"].toString()
-            val netID: String = email.split("@")[0]
-            newRows.add(listOf(name, email, netID))
-        }
-    }
-
-    return newRows
-}
-
 fun main() {
     print("Enter the filepath of the csv: ")
     val filePath: String = readLine()!!
-    writeToCSV("data/data.csv", createNewCSV(readFromCSV(filePath)))
+    val parsedFilePath: String = "data/Parsed_Data.csv"
+    writeToCSV(parsedFilePath, createNewCSV(readFromCSV(filePath)))
 }
